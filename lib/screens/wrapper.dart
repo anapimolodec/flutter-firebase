4c5310799b8f636ecc96827c07790e4e0be06f93
Aiaru <aiaru.mukhamedyarova@gmail.com>
2023-10-04 13:55:43 +0900

    firebase connected

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:ff/screens/authenticate/authenticate.dart';
+// import 'package:ff/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return either the Home or Authenticate widget
+    return Authenticate();
+  }
+}
